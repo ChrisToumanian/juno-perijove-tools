97ce9bc53b660a7c8f4b2135e7ea1b5fc372aea5
Christopher Toumanian <chris@MacBook-Pro.lan>
2020-07-29 08:11:10 -0700

    implemented horizons.py

--- horizons.py
+++ horizons.py
@@ -5,4 +5,47 @@ import urllib.request
 import math
 from datetime import datetime, timedelta
 
+def get_julian_datetime(date):
+	# Ensure correct format
+	if not isinstance(date, datetime):
+		raise TypeError('Invalid type for parameter "date" - expecting datetime')
+	elif date.year < 1801 or date.year > 2099:
+		raise ValueError('Datetime must be between year 1801 and 2099')
 
+	# Perform the calculation
+	julian_datetime = 367 * date.year - int((7 * (date.year + int((date.month + 9) 
+	/ 12.0))) / 4.0) + int((275 * date.month) / 9.0) + date.day + 1721013.5 
+	+ (date.hour + date.minute / 60.0 + date.second / math.pow(60,2)) / 24.0 
+	- 0.5 * math.copysign(1, 100 * date.year + date.month - 190002.5) + 0.5
+
+	return julian_datetime
+
+class HorizonsRequest:
+	def __init__(self, center, target, datetime, quantities):
+		self.keys = {
+			"CENTER": center,
+			"COMMAND": target,
+			"QUANTITIES": quantities,
+			"TLIST": str(get_julian_datetime(datetime)),
+			"MAKE_EPHEM": "YES",
+			"OBJ_DATA": "NO",
+			"TABLE_TYPE": "OBSERVER",
+			"CAL_FORMAT": "BOTH",
+			"ANG_FORMAT": "DEG",
+			"CSV_FORMAT": "YES"
+		}
+		self.response = []
+	
+	def send(self):
+		# create request
+		request = "https://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=1"
+		for key in self.keys:
+			request += "&" + key + "='" + self.keys[key] + "'"
+
+		# send request
+		file = urllib.request.urlopen(request)
+		for line in file:
+			self.response.append(line.decode('utf-8').replace('\n', ''))
+
+	def get_response(self):
+		return self.response
---
+++ test-horizons.py
@@ -0,0 +1,15 @@
+#! /usr/local/bin/python
+
+import horizons
+from datetime import datetime
+
+date = datetime.utcnow()
+
+request = horizons.HorizonsRequest("672@399", "599", date, "2,20,21")
+
+request.send()
+
+response = request.get_response()
+
+for line in response:
+	print(line)
